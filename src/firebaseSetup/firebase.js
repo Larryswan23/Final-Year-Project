f80b3b2edd8d39318adb516d3d774c5bfcd4f40b
Larryswan23 <larryswan23@icloud.com>
2024-04-26 09:11:27 +0100

    final commit

--- src/firebaseSetup/firebase.js
+++ src/firebaseSetup/firebase.js
@@ -3,7 +3,7 @@ import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from 'firebase/auth';
 
-// Your web app's Firebase configuration
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDN92ukxV8OErTstq6TW9sA_7O_sIM-RSU",
